44dfff9ec047ea7efd09b53f93b655153d56285e
Nymann <nymannjakobsen@gmail.com>
2015-05-14 01:59:19 +0200

    JUnit test implementation

---
+++ src/data/BootTest.java
@@ -0,0 +1,14 @@
+package data;
+
+import static org.junit.Assert.*;
+
+/**
+ * By Kristian, you can find me on Github at Github.com/Nymann.
+ */
+public class BootTest {
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+
+    }
+}
