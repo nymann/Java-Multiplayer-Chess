7ed3b4c051adf7e14b0f52ec6263723a45df1e7f
Nymann <nymannjakobsen@gmail.com>
2015-05-13 23:46:28 +0200

    Castling recognition is bugged.

--- src/data/rules/KingRules.java
+++ src/data/rules/KingRules.java
@@ -263,6 +263,60 @@ public class KingRules {
         } else {
             // Kings is black.
 
+            // Checking for the ability to castle.
+            if ((rowOfKingInQuestion == 0) && (colOfKingInQuestion == 4)) {
+                System.out.println("YO");
+                // The king is on it's start square, now checking if the two squares to the right of the king is empty and the third is a rook.
+                kingsideCastleCheck:
+                for (int i = 1; i <= 2; i++) {
+                    if (position[rowOfKingInQuestion][colOfKingInQuestion + i].equals("ES")) {
+                        if (position[rowOfKingInQuestion][colOfKingInQuestion + i + 1].equals("WR")) {
+                            try (BufferedReader bufferedReader = new BufferedReader(new FileReader("res/log.txt"))) {
+                                String line;
+
+                                while ((line = bufferedReader.readLine()) != null) {
+                                    if (line.contains("e8") || line.contains("h8")) {
+                                        System.out.println("kingside castling is not an option");
+                                        break kingsideCastleCheck;
+                                    }
+                                }
+
+                                System.out.println("Kingside castling is an option!");
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+
+                queensideCastleCheck:
+                for (int i = 1; i < 3; i++) {
+                    if (position[rowOfKingInQuestion][colOfKingInQuestion - i].equals("ES")) {
+                        if (position[rowOfKingInQuestion][colOfKingInQuestion - i - 1].equals("ES")) {
+                            if (position[rowOfKingInQuestion][colOfKingInQuestion - i - 2].equals("WR")) {
+                                try (BufferedReader bufferedReader = new BufferedReader(new FileReader("res/log.txt"))) {
+                                    String line;
+
+                                    while ((line = bufferedReader.readLine()) != null) {
+                                        if (line.contains("e8") || line.contains("a8")) {
+                                            System.out.println("kingside castling is not an option");
+                                            break queensideCastleCheck;
+                                        }
+                                    }
+
+                                    System.out.println("Queenside castling is an option!");
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
             // Checking the row 0, which is the black kings start row.
             if ((rowOfKingInQuestion == 0) && ((colOfKingInQuestion != 0) && (colOfKingInQuestion != 7))) {
                 // From this criteria there is up to 5 legal moves for every king position.
