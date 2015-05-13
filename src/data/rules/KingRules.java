fa19cbb1f41a5019da0d8f80c8bceb8609e677c4
Nymann <nymannjakobsen@gmail.com>
2015-05-13 23:32:14 +0200

    White castling kingside + queenside option is now printed in console, yet to be implemented.

--- src/data/rules/KingRules.java
+++ src/data/rules/KingRules.java
@@ -1,5 +1,9 @@
 package data.rules;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +21,62 @@ public class KingRules {
 
         if (colorOfKingIsWhite) {
             // King is white.
+
+            // Checking for the ability to castle kingside.
+            if ((rowOfKingInQuestion == 7) && (colOfKingInQuestion == 4)) {
+                // The king is on it's start square, now checking if the two squares to the right of the king is empty and the third is a rook.
+                kingsideCastleCheck:
+                for (int i = 1; i <= 2; i++) {
+                    if (position[rowOfKingInQuestion][colOfKingInQuestion + i].equals("ES")) {
+                        if (position[rowOfKingInQuestion][colOfKingInQuestion + i + 1].equals("WR")) {
+                            try (BufferedReader bufferedReader = new BufferedReader(new FileReader("res/log.txt"))) {
+                                String line;
+
+                                while ((line = bufferedReader.readLine()) != null) {
+                                    if (line.contains("e1") || line.contains("h1")) {
+                                        System.out.println("kingside castling is not an option");
+                                        break kingsideCastleCheck;
+                                    }
+                                }
+
+                                System.out.println("Kingside castling is an option!");
+
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+
+                queensideCastleCheck:
+                for (int i = 1; i < 3; i++) {
+                    if (position[rowOfKingInQuestion][colOfKingInQuestion - i].equals("ES")) {
+                        if (position[rowOfKingInQuestion][colOfKingInQuestion - i - 1].equals("ES")) {
+                            if (position[rowOfKingInQuestion][colOfKingInQuestion - i - 2].equals("WR")) {
+                                try (BufferedReader bufferedReader = new BufferedReader(new FileReader("res/log.txt"))) {
+                                    String line;
+
+                                    while ((line = bufferedReader.readLine()) != null) {
+                                        if (line.contains("e1") || line.contains("a1")) {
+                                            System.out.println("kingside castling is not an option");
+                                            break queensideCastleCheck;
+                                        }
+                                    }
+
+                                    System.out.println("Queenside castling is an option!");
+
+                                } catch (FileNotFoundException e) {
+                                    e.printStackTrace();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 
             // Checking the row 0, which is the black kings start row.
             if ((rowOfKingInQuestion == 0) && ((colOfKingInQuestion != 0) && (colOfKingInQuestion != 7))) {
